d92b5046b07f5dc0cdc484972070dcb5ba75ed70
Suhail Abood <suhail.abood@gmail.com>
2020-10-24 18:47:21 +0100

    Add support to extract expiry date from jwt

--- src/constants.ts
+++ src/constants.ts
@@ -27,10 +27,37 @@ export async function forceRefreshUser() {
   return user;
 }
 
+export function parseJwtToken(token: string) {
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+}
+
+export function getIsExpiredFromJwt(token: string) {
+  const parsedToken = parseJwtToken(token);
+  return typeof parsedToken.exp !== "undefined"
+    ? new Date(parsedToken.exp * 1000)
+    : undefined;
+}
+
 export async function getUserSilently() {
   let user = await userManager.getUser();
 
-  if (!user || user.expired) {
+  const expired =
+    typeof user.expired === "undefined"
+      ? getIsExpiredFromJwt(user.id_token || user.access_token)
+      : user.expired;
+
+  if (!user || expired) {
     user = null;
     await userManager.removeUser();
     try {
